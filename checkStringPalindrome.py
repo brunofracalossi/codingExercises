40822a169877eb116ad7fec5f0b1a55325054198
Bruno Fracalossi Ferreira <bruno.fracalossi@wal-mart.com>
2014-11-10 13:41:07 -0200

    palindrome exercise

---
+++ checkStringPalindrome.py
@@ -0,0 +1,11 @@
+string = "abba"
+size = len(string)
+aux_size = size - 1
+is_palindrome = True
+
+for i in xrange(size / 2):
+	if string[i] != string[aux_size]:
+		is_palindrome = False
+	aux_size = aux_size - 1
+
+print is_palindrome
