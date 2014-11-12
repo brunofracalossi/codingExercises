6ccf72612296f8272f34c5eab6a68019cd16569b
Bruno Fracalossi Ferreira <bruno.fracalossi@wal-mart.com>
2014-11-12 14:18:40 -0200

    square root exercise (not optimized version)

---
+++ squareRoot.py
@@ -0,0 +1,31 @@
+import sys
+
+number = float(sys.argv[1])
+
+square_root = number
+find_sq_root = False
+no_square_root = False
+
+while not find_sq_root:
+	sq = round(square_root * square_root, 3)
+	print sq
+	if sq == number:
+		find_sq_root = True
+	else:
+		if square_root < 1:
+			square_root = square_root + 0.0001
+		else:
+			square_root = square_root - 0.0001
+
+	if square_root <= 0:
+		find_sq_root = True
+		no_square_root = True
+
+	if sq < number and number > 1:
+		find_sq_root = True
+		no_square_root = True
+
+if no_square_root:
+	print "There is no square root for num " + str(number)
+else:
+	print "Square root of " + str(number) + " is " + str(round(square_root,3))
