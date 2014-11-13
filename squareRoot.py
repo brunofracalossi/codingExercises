ebcc3e8adb464a4cda8646d3f6b443b74ff2b1b7
brunofracalossi <brunofracalossi@gmail.com>
2014-11-13 08:56:09 -0200

    fix in bug

--- squareRoot.py
+++ squareRoot.py
@@ -7,15 +7,18 @@ find_sq_root = False
 no_square_root = False
 
 while not find_sq_root:
-	sq = round(square_root * square_root, 3)
+	if number >= 1:
+		sq = round(square_root * square_root,1)
+	else:
+		sq = round(square_root * square_root,3)
 	print sq
 	if sq == number:
 		find_sq_root = True
 	else:
 		if square_root < 1:
-			square_root = square_root + 0.0001
+			square_root = square_root + 0.001
 		else:
-			square_root = square_root - 0.0001
+			square_root = square_root - 0.001
 
 	if square_root <= 0:
 		find_sq_root = True
